b49433176a2c9e80d6fdbec048cefcccb88139d9
3oc <TAKAHIDE@H.local>
2015-04-27 21:43:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ishalog::Application.config.secret_key_base = 'fc0da5c556a6c746aaa95a5a70773ee486493333303b1d6017dc69565ca78f5813364d904b678d5f56b73494514cd7de45e712da8ba1647ef3447409fb9e24b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ishalog::Application.config.session_store :cookie_store, key: '_ishalog_session'
