f4b2a460ab803aed365576a91555248befcb6396
3oc <TAKAHIDE@H.local>
2015-04-27 22:24:30 +0900

    something

--- vendor/bundle/ruby/2.0.0/gems/json-1.8.2/ext/json/siteconf20150427-24964-s2zq0i.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20150427-24964-x4u91e"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
--- vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.10/ext/sqlite3/siteconf20150427-24964-1t9egam.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20150427-24964-1t1p6g4"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
