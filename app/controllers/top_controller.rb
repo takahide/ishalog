bcf12e8c0c98d69c4c989f6de8296f75609e127c
3oc <TAKAHIDE@H.local>
2015-05-06 00:53:40 +0900

    something

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,11 @@
 class TopController < ApplicationController
   def index
-    token = current_user.token
-    @graph = Koala::Facebook::API.new(token)
-    @token = @graph.get_picture('me')
+    if current_user.present?
+      token = current_user.token
+      @graph = Koala::Facebook::API.new(token)
+      @token = @graph.get_picture('me')
+    else 
+      @token = nil
+    end
   end
 end
