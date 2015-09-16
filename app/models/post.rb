77ccd04513e3e4ea8b22a7c7f32f11323c996da7
takahide <h@takahi.de>
2015-09-17 01:10:32 +0900

    something

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   def body_with_html
     self.body
     .gsub(/(\r\n|\r|\n)/, "<br>")
-    .gsub(/(https?:\/\/[a-zA-Z0-9\-_.\/]*)/, '<a href="\1">\1</a>')
+    .gsub(/(https?:\/\/[a-zA-Z0-9\-=_.\?\/]*)/, '<a href="\1">\1</a>')
   end
   def introduced_clinics
     clinic_ids = self.clinics.split(",")
