00bd31ee4fb7d9cec606f502085694d3bbed83a1
takahide <h@takahi.de>
2015-09-16 17:43:11 +0900

    something

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Base
     .gsub(/(\r\n|\r|\n)/, "<br>")
     .gsub(/(https?:\/\/[a-zA-Z0-9\-_.\/]*)/, '<a href="\1">\1</a>')
   end
+  def introduced_clinics
+    clinic_ids = self.clinics.split(",")
+    Clinic.where("id in (?)", clinic_ids)
+  end
 end
